2335f05c218e6789bdde55637c207acd0e8cb3cc
Maneesh Sistla <maneesh.m2wifcs@gmail.com>
2019-01-31 01:17:47 +0800

    Dropbox uploading script

---
+++ sctask/upload.py
@@ -0,0 +1,10 @@
+import dropbox
+
+access_token = 'Qdc4fZ3xgNAAAAAAAAAArEkhOwxVt9_4YnA_pwDVoabx3ru-hiFZuy2Qio0Dl-Tm'
+file_from = 'players.csv'
+file_to = '/dropbox_py/players.csv'
+
+dbx = dropbox.Dropbox(access_token)
+
+with open(file_from, 'rb') as f:
+		dbx.files_upload(f.read(), file_to)

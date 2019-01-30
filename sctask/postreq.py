877adccde7ad0c719833a4cdcf4fc78955a97739
maneeshsistla8 <46049177+maneeshsistla8@users.noreply.github.com>
2019-01-30 13:31:29 +0530

    Update postreq.py

--- sctask/postreq.py
+++ sctask/postreq.py
@@ -1,4 +1,4 @@
 import requests
 
-payload = {'name':'Gonzalo Higuain', 'nation': 'Belgium', 'stats':'https://www.whoscored.com/Players/19729/Show/Gonzalo-Higua%C3%ADn'}
+payload = {'name':'Gonzalo Higuain', 'nation': 'Argentina', 'stats':'https://www.whoscored.com/Players/19729/Show/Gonzalo-Higua%C3%ADn'}
 r = requests.post("http://127.0.0.1:8000/players/", data=payload)

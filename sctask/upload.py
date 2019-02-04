3d249f1f8441ca04f81b664c1bb9ee93ef3bf2b6
Maneesh Sistla <maneesh.m2wifcs@gmail.com>
2019-02-04 16:57:14 +0800

    Modified upload.py and tested by adding player

--- sctask/upload.py
+++ sctask/upload.py
@@ -7,4 +7,4 @@ file_to = '/dropbox_py/players.csv'
 dbx = dropbox.Dropbox(access_token)
 
 with open(file_from, 'rb') as f:
-		dbx.files_upload(f.read(), file_to)
+		dbx.files_upload(f.read(), file_to, mode=dropbox.files.WriteMode.overwrite)
